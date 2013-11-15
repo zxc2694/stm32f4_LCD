e04d43e6d20268129fd4a2c187f0dc60f0b8777f
zxc2694 <zxc2694zxc2694@yahoo.com.tw>
2013-11-15 23:01:24 +0800

    Add Delay and let Pin12 Pin9 sequentially brighten

--- main.c
+++ main.c
@@ -4,22 +4,25 @@
 #include "main.h"
 #include "stm32f4xx_conf.h"
 
+#define  Delay(n)  while((n)--)
+
 void RCC_Configuration(void);
 void GPIO_Configuration(void);
+void GPIO_Configuration2(void);
 
 int main(void)
 {
-	
+	vu32 n=20000000;
 	RCC_Configuration();
-	GPIO_Configuration(); 
-	while(1){
-		GPIO_SetBits(GPIOD, GPIO_Pin_9);
-		GPIO_SetBits(GPIOD, GPIO_Pin_12);
-		vTaskDelay(200);
-	//	GPIO_ResetBits(GPIOD,GPIO_Pin_9);
-	//	GPIO_ResetBits(GPIOD,GPIO_Pin_12);
-	//	vTaskDelay(1000);
-	}
+	GPIO_Configuration();
+
+	GPIO_ResetBits(GPIOD, GPIO_Pin_9);
+	GPIO_SetBits(GPIOD, GPIO_Pin_12);
+	Delay(n);
+	
+	GPIO_SetBits(GPIOD,GPIO_Pin_9);
+	GPIO_ResetBits(GPIOD,GPIO_Pin_12);
+	Delay(n);
 }
 
 void RCC_Configuration(void)
@@ -29,15 +32,14 @@ void RCC_Configuration(void)
 
 void GPIO_Configuration(void)
 {
-	GPIO_InitTypeDef GPIO_InitStructure;
-	GPIO_InitStructure.GPIO_Pin=GPIO_Pin_9 | GPIO_Pin_12;
-	GPIO_InitStructure.GPIO_Speed=GPIO_Speed_50MHz;
-	GPIO_InitStructure.GPIO_Mode=GPIO_Mode_OUT;
-	GPIO_InitStructure.GPIO_OType=GPIO_OType_PP;
-	GPIO_InitStructure.GPIO_PuPd=GPIO_PuPd_NOPULL;
-	GPIO_Init(GPIOD,&GPIO_InitStructure);
+	GPIO_InitTypeDef G;
+	G.GPIO_Pin=GPIO_Pin_9 | GPIO_Pin_12;
+	G.GPIO_Speed=GPIO_Speed_50MHz;
+	G.GPIO_Mode=GPIO_Mode_OUT;
+	G.GPIO_OType=GPIO_OType_PP;
+	G.GPIO_PuPd=GPIO_PuPd_NOPULL;
+	GPIO_Init(GPIOD,&G);
 }
-
 
 void vApplicationTickHook()
 {
