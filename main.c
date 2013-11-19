48c963e9e4279d0ade5a06d2db1e236ad46d0ff8
zxc2694 <zxc2694zxc2694@yahoo.com.tw>
2013-11-19 21:53:54 +0800

    Using another delay method to realize LED flicker

--- main.c
+++ main.c
@@ -4,42 +4,49 @@
 #include "main.h"
 #include "stm32f4xx_conf.h"
 
-#define  Delay(n)  while((n)--)
+void Delay(__IO unsigned long num)
+{
+	while(num--);
+}
 
 void RCC_Configuration(void);
 void GPIO_Configuration(void);
-void GPIO_Configuration2(void);
 
 int main(void)
 {
-	vu32 n=20000000;
+	__IO unsigned long n=0xE00000;
 	RCC_Configuration();
 	GPIO_Configuration();
-
-	GPIO_ResetBits(GPIOD, GPIO_Pin_9);
-	GPIO_SetBits(GPIOD, GPIO_Pin_12);
-	Delay(n);
-	
-	GPIO_SetBits(GPIOD,GPIO_Pin_9);
-	GPIO_ResetBits(GPIOD,GPIO_Pin_12);
-	Delay(n);
+	while(1)
+	{
+		GPIO_ResetBits(GPIOD, GPIO_Pin_13);
+		GPIO_SetBits(GPIOD, GPIO_Pin_15);
+		Delay(n);
+		
+		GPIO_SetBits(GPIOD, GPIO_Pin_13);
+		GPIO_ResetBits(GPIOD, GPIO_Pin_15);
+		Delay(n);
+	}
 }
 
 void RCC_Configuration(void)
 {
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD,ENABLE);
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC,ENABLE);
 }
 
 void GPIO_Configuration(void)
 {
 	GPIO_InitTypeDef G;
-	G.GPIO_Pin=GPIO_Pin_9 | GPIO_Pin_12;
+	G.GPIO_Pin=GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
 	G.GPIO_Speed=GPIO_Speed_50MHz;
 	G.GPIO_Mode=GPIO_Mode_OUT;
 	G.GPIO_OType=GPIO_OType_PP;
-	G.GPIO_PuPd=GPIO_PuPd_NOPULL;
+	G.GPIO_PuPd=GPIO_PuPd_UP;
 	GPIO_Init(GPIOD,&G);
+
 }
+
 
 void vApplicationTickHook()
 {
